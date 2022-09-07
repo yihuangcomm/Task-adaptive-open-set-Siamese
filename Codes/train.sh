6ef40ef9a63d9196712b6111702cfa47dfe21443
Yi Huang <37393578+yihuangcomm@users.noreply.github.com>
2022-09-08 00:11:16 +1000

    Update train.sh

--- Codes/train.sh
+++ Codes/train.sh
@@ -5,7 +5,7 @@ python3 train_network.py --train_path ../train_train --test_path ../val --gpu_id
 
 #python3 train_network_original.py --train_path ../train_train --test_path ../val --gpu_ids 0  --model_path ../siamese_model_40_10_10_new  --dropout_p 0.0 --dropout_n 0.0 --hloss_alpha 0.0 --feature_n 2 --features 0,20
 
-#python3 train_omni.py --train_path ../omniglot/python/images_background --test_path ../omniglot/python/images_validation --backbone conv --gpu_ids 0 --dropout_p 0.0 --dropout_n 0.0 --hloss_alpha 0.3 --model_path ../siamese_model_omni_new --is_hloss True 
+#python3 train_omni.py --train_path ../omniglot/python/images_background --test_path ../omniglot/python/images_validation --gpu_ids 0 --dropout_p 0.0 --dropout_n 0.0 --hloss_alpha 0.3 --model_path ../siamese_model_omni_new --is_hloss True 
 
 
 
